a8213e34a6759f8368da32ce496971605ebf6461
Max Falkenstein <mrfalken@iu.edu>
2018-05-29 11:09:30 -0400

    add submit handler to add github user to the URL

--- src/Github.js
+++ src/Github.js
@@ -3,6 +3,20 @@ import React, { Component } from 'react'
 import './Github.css'
 
 class Github extends Component {
+  state = {
+    username: '',
+  }
+
+  handleChange = (ev) => {
+    this.setState({ username: ev.target.value })
+  }
+
+  handleSubmit = (ev) => {
+    ev.preventDefault()
+    this.props.history.push(`/github/${this.state.username}`)
+    this.setState({ username: ''})
+  }
+
   render() {
     return (
       <div className="Github">
@@ -11,9 +25,13 @@ class Github extends Component {
           src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"
           alt="GitHub"
         />
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div>
-            <input type="text "/>
+            <input
+              type="text"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
           </div>
           <div>
             <button type="submit">Look up GitHub user</button>
